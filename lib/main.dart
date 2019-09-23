8e0116a8664f72178afb25f5ad53526b87e84cb2
Uziel Trujillo <uziel.trujillo@gmail.com>
2019-09-23 10:52:30 -0500

    Eliminando el StatefulWidget de main.dart e implementando Scaffold a mi StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -11,57 +11,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(title: 'Taller Cytiis 2019'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Presionaste:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            Text(
-              'Veces',
-            )
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Taller Cytiis 2019')
         ),
+        body: Center(
+          child: Text('El mitico hola mundo')
+        )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.plus_one),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
