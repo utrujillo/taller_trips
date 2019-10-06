7f240d73724b01d0d828d6a91f0db17473cacc35
Uziel Trujillo <uziel.trujillo@gmail.com>
2019-10-06 15:50:40 -0500

    Agregando mayor interacción al boton de favoritos 	modified:   lib/inicio/botonFav.dart

--- lib/inicio/botonFav.dart
+++ lib/inicio/botonFav.dart
@@ -6,11 +6,25 @@ class BotonFav extends StatefulWidget{
 }
 
 class _BotonFav extends State<BotonFav>{
+  bool estaActivo = false;
+  IconData icono = Icons.favorite_border;
+  String mensaje = "";
 
   void favPresionado(){
+    
+    setState( () => estaActivo = !estaActivo );
+
+    if( estaActivo ){
+      icono = Icons.favorite;
+      mensaje = "Agregaste a favoritos";
+    }else{
+      icono = Icons.favorite_border;
+      mensaje = "Eliminaste de favoritos";
+    }
+
     Scaffold.of(context).showSnackBar(
       SnackBar(
-        content: Text("Agregaste a favoritos")
+        content: Text(mensaje)
       )
     );
   }
@@ -23,7 +37,7 @@ class _BotonFav extends State<BotonFav>{
       mini: true,
       tooltip: "Fav",
       child: Icon(
-        Icons.favorite_border
+        icono
       ),
       onPressed: favPresionado,
     );
